5e209a8d1bc96378fb2cc2f2d64159aad6a52e83
Jared Murphy <jaredcmurphy@gmail.com>
2018-02-11 13:29:14 -0500

    clean up exercises folder

--- completed_exercises/reversestring/index.js
+++ completed_exercises/reversestring/index.js
@@ -6,12 +6,7 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-  return str.split('').reduce((rev, char) => char + rev, '');
-}
-
-module.exports = reverse;
-
+// convert to array and reverse
 // function reverse(str) {
 //   return str
 //     .split('')
@@ -19,12 +14,27 @@ module.exports = reverse;
 //     .join('');
 // }
 
+// for loop - descending counter
 // function reverse(str) {
 //   let reversed = '';
-//
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
+
+// es6 for loop syntax
+// function reverse(str) {
+//   let reversed = '';
 //   for (let character of str) {
 //     reversed = character + reversed;
 //   }
-//
 //   return reversed;
 // }
+
+// reduce
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+module.exports = reverse;
