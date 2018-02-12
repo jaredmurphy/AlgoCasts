9bf70092c53d7699bbad64a59c380c40833d8234
Jared Murphy <jaredcmurphy@gmail.com>
2018-02-11 21:47:24 -0500

    adds max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// ##### Create character count, iterate to find letter with highest number
+function maxChar(str) {
+  const counts = str.split('').reduce((prev, curr) => {
+    prev[curr] ? prev[curr]++ : (prev[curr] = 1);
+    return prev;
+  }, {});
+
+  let topChar = '';
+  let topCount = 0;
+
+  for (let key in counts) {
+    if (counts[key] > topCount) {
+      topChar = key;
+      topCount = counts[key];
+    }
+  }
+
+  return topChar;
+}
 
 module.exports = maxChar;
