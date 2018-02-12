d42407c05d6ca087749f3eaa8c1d0da3be1b829a
Jared Murphy <jaredcmurphy@gmail.com>
2018-02-11 21:48:43 -0500

    move answer for max chars to only cmpleted folder

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,24 +5,6 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-// ##### Create character count, iterate to find letter with highest number
-function maxChar(str) {
-  const counts = str.split('').reduce((prev, curr) => {
-    prev[curr] ? prev[curr]++ : (prev[curr] = 1);
-    return prev;
-  }, {});
-
-  let topChar = '';
-  let topCount = 0;
-
-  for (let key in counts) {
-    if (counts[key] > topCount) {
-      topChar = key;
-      topCount = counts[key];
-    }
-  }
-
-  return topChar;
-}
+function maxChar(str) {}
 
 module.exports = maxChar;
